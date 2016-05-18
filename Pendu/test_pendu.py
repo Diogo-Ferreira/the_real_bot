8bd31a8f11153808e80b0109f311494d3c6a33ad
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2016-05-18 14:53:06 +0100

    ajout tests

---
+++ Pendu/test_pendu.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Pendu import Pendu
+
+
+class TestPendu(TestCase):
+
+    def test_is_game_won(self):
+        #Test victoire
+        pendu = Pendu("tata",10)
+        pendu.check_letter("t")
+        pendu.check_letter("a")
+        self.assertTrue(pendu.is_game_won())
+
+        #Test défaite
+        pendu = Pendu("tata",10)
+        for i in "dfeghttzzs":
+            pendu.check_letter(i)
+
+        self.assertFalse(pendu.is_game_won())
