7dd07847638059c8315246cfa5a315f345f38ffd
LumenH <aurelie.debrot@he-arc.ch>
2016-06-03 13:37:47 +0200

    ajout de l aide

--- Pendu/PenduController.py
+++ Pendu/PenduController.py
@@ -12,7 +12,12 @@ class PenduController:
     def interpret_user_input(self,user_input):
         """Interprète l'entrée de l'utilisateur, pour savoir ce qu'il veut"""
         if user_input == "aide moi":
-            return "affichage aide..."
+            return "Pour commencer une partie tapez : nouvelle partie \n" \
+                   "Vous pouvez faire suivre cette commande d'une catégorie de votre choix\n" \
+                   "Les catégories disponibles sont les suivants : gen (catégorie par défaut)\n" \
+                   "Pour terminer une partie tapez : fin de partie\n" \
+                   "Une fois que la partie a commencé tapez la lettre de votre choix\n" \
+                   "Vous pouvez taper un mot en entier si vous pensez l'avoir devnié."
         elif user_input == "fin de partie":
             self.game_on = False
             return "Bye Bye"
